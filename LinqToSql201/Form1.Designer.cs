f37397672ba031ecbdba5f9558bd0049e80f705b
ousra <ousra@DESKTOP-1LRJ0HP>
2022-03-07 09:41:56 +0100

    Ajoutez des fichiers projet.

---
+++ LinqToSql201/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace LinqToSql201
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(309, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(479, 547);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(291, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "liste des Editeurs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(291, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "liste des villes Editeurs";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(291, 42);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "liste des Editeurs de Paris";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(291, 42);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "liste des Editeurs de Paris (Methode)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 204);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(291, 42);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "liste des Editeurs de Paris";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(744, 562);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 607);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LinqToSql201/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSql201
+{
+    public partial class Form1 : Form
+    {
+      librairieDataContext db = new librairieDataContext();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var c = db.EDITEUR;
+            dataGridView1.DataSource = c;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+          //  afficher la liste des noms et des villes de tous les editeurs
+            var ed = from edi in db.EDITEUR
+                     select new { edi.NOMED, edi.VILLEED };
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //afficher la liste des noms et des villes des editeurs qui habitent a paris
+
+            // avec ==
+            //var ed = from edi in db.EDITEUR
+            //         where edi.VILLEED == "paris"
+            //         select new { edi.NOMED, edi.VILLEED };
+
+            //avec equals
+            var ed = from edi in db.EDITEUR
+                     where edi.VILLEED.Equals("paris")
+                     select new { edi.NOMED, edi.VILLEED };
+
+           
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //afficher la liste des noms et des villes des editeurs qui habitent a paris
+            //avec le tri descendant sur les nom
+
+            //var x = db.EDITEUR;
+         
+            //var a = x.Where( c => c.VILLEED == "paris");
+            //var b = a.OrderBy(c => c.NOMED); //OrderByDescending
+            //var ed = b.Select(c => new { c.NOMED, c.VILLEED });
+
+
+            var ed = db.EDITEUR
+                .Where(c => c.VILLEED == "paris")
+                .OrderBy(c => c.NOMED) //OrderByDescending
+                .Select(c => new { c.NOMED, c.VILLEED });
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // liste des editeurs et des ouvrages
+
+
+            //produit cartesien
+            //var ed = (from edi in db.EDITEUR
+            //          from ouv in db.OUVRAGE
+            //          where edi.NOMED == ouv.NOMED
+            //          select new { edi.NOMED, ouv.NOMOUVR }).Distinct();
+
+
+            //jointure interne (inner join)
+            var ed = (from edi in db.EDITEUR
+                      join ouv in db.OUVRAGE
+                      on edi.NOMED equals ouv.NOMED
+                      select new { edi.NOMED, ouv.NOMOUVR }).Distinct();
+            label1.Text = ed.Count().ToString();
+
+
+            dataGridView1.DataSource = ed;
+
+
+        }
+    }
+}
